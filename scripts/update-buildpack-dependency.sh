28ca8d9bcdf051cc57b98c1a956ba305b192723d
Ben Hale <bhale@vmware.com>
2020-10-06 09:43:38 -0700

    Dependency Update Pipelines
    This change updates the buildpack with dependency update pipelines for all of
    the buildpack's dependencies.
    
    Signed-off-by: Ben Hale <bhale@vmware.com>

--- scripts/update-buildpack-dependency.sh
+++
@@ -1,7 +0,0 @@
-sha256() {
-  cat "${ROOT}"/dependency/sbt-*.tgz.sha256 | cut -f 1 -d ' '
-}
-
-uri() {
-  echo "https://github.com/sbt/sbt/releases/download/v$(cat "${ROOT}"/dependency/version)/sbt-$(cat "${ROOT}"/dependency/version).tgz"
-}
